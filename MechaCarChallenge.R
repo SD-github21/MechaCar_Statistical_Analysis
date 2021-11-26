39a2f5dbbbb86bcfaa1a01fa3b525946ddd138c1
SD-github21 <sapandonde.gmail.com>
2021-11-26 17:08:01 -0600

    Adding Revised MechaCarChallenge.R file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,3 +4,13 @@ data <- read.csv("MechaCar_mpg.csv", check.names=F,stringsAsFactors = F)
 head(data)
 lm(mpg ~ vehicle_length + vehicle_weight  +  spoiler_angle + ground_clearance + AWD,data=data) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight  +  spoiler_angle + ground_clearance + AWD,data=data)) #generate summary statistics
+
+# Deliverable 2
+suscoildata <- read.csv("Suspension_Coil.csv", check.names=F,stringsAsFactors = F)
+head(suscoildata)
+
+total_summary <- summarize(suscoildata, Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI)) #generate summary stats for PSI
+total_summary
+
+lot_summary <- suscoildata  %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep') 
+lot_summary
