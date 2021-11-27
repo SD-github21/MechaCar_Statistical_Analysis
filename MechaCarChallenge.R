4963887f27517f1579a8bf843e2499814482f789
SD-github21 <sapandonde.gmail.com>
2021-11-27 14:37:08 -0600

    Adding final MechaCarChallenge.R file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,3 +14,26 @@ total_summary
 
 lot_summary <- suscoildata  %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep') 
 lot_summary
+
+# Deliverable #3
+PSItest <- read.csv("Suspension_Coil.csv", check.names=F,stringsAsFactors = F)
+head(PSItest)
+
+#Conducting a 1 sample t test comparing the mean PSI  of all manufacturing lots to the mean PSI in the population of cars
+t.test((PSItest$PSI),mu=1500) #compare sample versus population means
+
+#Conducting 1 sample t tests comparing the mean PSI for each lot against the mean PSI of the population
+# Lot1
+PSILot1 <- subset(PSItest, Manufacturing_Lot == "Lot1")
+PSILot1
+t.test((PSILot1$PSI),mu=1500) #compare Lot 1 PSI versus population means
+
+#Lot2
+PSILot2 <- subset(PSItest, Manufacturing_Lot == "Lot2")
+PSILot2
+t.test((PSILot2$PSI),mu=1500) #compare Lot 2 PSI versus population means
+
+#Lot3
+PSILot3 <- subset(PSItest, Manufacturing_Lot == "Lot3")
+PSILot3
+t.test((PSILot3$PSI),mu=1500) #compare Lot 3 PSI versus population means
